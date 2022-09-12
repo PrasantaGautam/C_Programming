5f048bd59bd0e6314a1086dfec48c23340290b90
Prashant Gautam <prashantgautam000000@gmail.com>
2022-09-12 19:52:26 +0545

    Array

---
+++ array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  // Datas are stored in array using comma separation
+  // int array_name[] = {datas)};
+  int numCollec[] = {25, 50, 75, 100};
+  
+  // We can print specific data using index number inside square bracket
+  printf("%d", numCollec[0]);
+ 
+  return 0;
+}
