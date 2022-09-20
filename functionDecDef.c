2362e4d375179fde4e2f1c9517fc7a915bf01b23
Prashant Gautam <86648752+PrasantaGautam@users.noreply.github.com>
2022-09-20 21:22:27 +0545

    Declaration and Definition

---
+++ functionDecDef.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Declaration of function 
+void funName();
+
+// Main
+int main() {
+  funName();  // calling the function
+  return 0;
+}
+
+// Definition of function 
+void funName() {
+  printf("Executed!");
+}
